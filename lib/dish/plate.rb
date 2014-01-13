1dc5407d3d67cd3265272c3fd950f5833d0205eb
Lasse Bunk <lasse@bunk.io>
2014-01-13 22:13:29 +0100

    Use method_missing for performance

--- lib/dish/plate.rb
+++ lib/dish/plate.rb
@@ -1,27 +1,30 @@
 module Dish
   class Plate
     def initialize(hash)
-      hash.each do |key, value|
-        dish_metaclass.send(:define_method, key) do
-          dish_convert_value(value)
-        end
-      end
+      @dish_original_hash = Hash[hash.map { |k, v| [k.to_s, v] }]
     end
 
     def method_missing(method, *args, &block)
       method = method.to_s
       if method.end_with?("?")
-        field = method[0..-2]
-        dish_check_for_presence(send(field))
+        key = method[0..-2]
+        dish_check_for_presence(key)
       else
-        nil
+        dish_get_value(method)
       end
     end
 
     private
 
-      def dish_check_for_presence(value)
-        !!value
+      attr_reader :dish_original_hash
+
+      def dish_get_value(key)
+        value = dish_original_hash[key]
+        dish_convert_value(value)
+      end
+
+      def dish_check_for_presence(key)
+        !!dish_get_value(key)
       end
 
       def dish_convert_value(value)
