6dc937354c79d8fa8e6665412ed426725a8fdc75
Lasse Bunk <lasse@bunk.io>
2014-01-31 18:42:44 +0100

    Underscore dish methods

--- lib/dish/plate.rb
+++ lib/dish/plate.rb
@@ -1,40 +1,40 @@
 module Dish
   class Plate
     def initialize(hash)
-      @dish_original_hash = Hash[hash.map { |k, v| [k.to_s, v] }]
+      @_dish_original_hash = Hash[hash.map { |k, v| [k.to_s, v] }]
     end
 
     def method_missing(method, *args, &block)
       method = method.to_s
       if method.end_with?("?")
         key = method[0..-2]
-        dish_check_for_presence(key)
+        _dish_check_for_presence(key)
       else
-        dish_get_value(method)
+        _dish_get_value(method)
       end
     end
 
     def as_hash
-      @dish_original_hash
+      @_dish_original_hash
     end
 
     private
 
-      attr_reader :dish_original_hash
+      attr_reader :_dish_original_hash
 
-      def dish_get_value(key)
-        value = dish_original_hash[key]
-        dish_convert_value(value)
+      def _dish_get_value(key)
+        value = _dish_original_hash[key]
+        _dish_convert_value(value)
       end
 
-      def dish_check_for_presence(key)
-        !!dish_get_value(key)
+      def _dish_check_for_presence(key)
+        !!_dish_get_value(key)
       end
 
-      def dish_convert_value(value)
+      def _dish_convert_value(value)
         case value
         when Hash then self.class.new(value)
-        when Array then value.map { |v| dish_convert_value(v) }
+        when Array then value.map { |v| _dish_convert_value(v) }
         else value
         end
       end
