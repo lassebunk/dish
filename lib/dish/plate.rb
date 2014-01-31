17b4ff093cb98c92259c398eccac78e7af11a568
Lasse Bunk <lasse@bunk.io>
2014-01-31 19:34:23 +0100

    Remove dish method prefix

--- lib/dish/plate.rb
+++ lib/dish/plate.rb
@@ -11,39 +11,39 @@ module Dish
     end
 
     def initialize(hash)
-      @_dish_original_hash = Hash[hash.map { |k, v| [k.to_s, v] }]
+      @_original_hash = Hash[hash.map { |k, v| [k.to_s, v] }]
     end
 
     def method_missing(method, *args, &block)
       method = method.to_s
       if method.end_with?("?")
         key = method[0..-2]
-        _dish_check_for_presence(key)
+        _check_for_presence(key)
       else
-        _dish_get_value(method)
+        _get_value(method)
       end
     end
 
     def as_hash
-      @_dish_original_hash
+      @_original_hash
     end
 
     private
 
-      attr_reader :_dish_original_hash
+      attr_reader :_original_hash
 
-      def _dish_get_value(key)
-        value = _dish_original_hash[key]
-        _dish_convert_value(value, self.class.coercions[key])
+      def _get_value(key)
+        value = _original_hash[key]
+        _convert_value(value, self.class.coercions[key])
       end
 
-      def _dish_check_for_presence(key)
-        !!_dish_get_value(key)
+      def _check_for_presence(key)
+        !!_get_value(key)
       end
 
-      def _dish_convert_value(value, coercion)
+      def _convert_value(value, coercion)
         case value
-        when Array then value.map { |v| _dish_convert_value(v, coercion) }
+        when Array then value.map { |v| _convert_value(v, coercion) }
         when Hash
           if coercion.is_a?(Proc)
             coercion.call(value)
