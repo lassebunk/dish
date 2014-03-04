2ec8316dc37eb8e97c4a8dfdf0b7562c5333b8a2
Erran Carey <me@errancarey.com>
2014-03-04 00:00:05 -0600

    Add RubyMotion support for #to_json
    + Check if JSON has been defined

--- lib/dish/plate.rb
+++ lib/dish/plate.rb
@@ -31,7 +31,15 @@ module Dish
     alias_method :as_hash, :to_h
 
     def to_json(*args)
-      to_h.to_json(*args)
+      # If we're using RubyMotion #to_json isn't available
+      if defined?(Motion::Project::Config)
+        # From BubbleWrap: https://github.com/rubymotion/BubbleWrap/blob/master/motion/core/json.rb#L30-L32
+        NSJSONSerialization.dataWithJSONObject(to_h, options: 0, error: nil).to_str
+      elsif defined?(JSON)
+        to_h.to_json(*args)
+      else
+        raise "#{self.class}#to_json depends on Hash#to_json. Try again after using `require 'json'`."
+      end
     end
 
     private
